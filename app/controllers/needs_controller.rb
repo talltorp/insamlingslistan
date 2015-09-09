79d052cec5084b4a8d772d32f1546a1bf574b3fd
Tobias Talltorp <tobias@talltorp.se>
2015-09-09 08:15:46 +0200

    Add admin_token to needs requests

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -1,21 +1,37 @@
 class NeedsController < ApplicationController
+  before_filter :validate_admin_token
+
   def create
-    @insamling = Insamling.find(params[:insamling_id])
-    @need = @insamling.needs.create(need_params)
-    redirect_to insamling_admin_url(@insamling.id, @insamling.admin_token)
+    @insamling = insamling
+    @need = insamling.needs.create(need_params)
+    redirect_to insamling_admin_url(insamling.id, insamling.admin_token)
   end
 
   def destroy
-    @insamling = Insamling.find(params[:insamling_id])
+    @insamling = insamling
     @need = Need.find(params[:id])
     @need.destroy
 
-    redirect_to insamling_admin_url(@insamling.id, @insamling.admin_token)
+    redirect_to insamling_admin_url(insamling.id, insamling.admin_token)
   end
 
   private
 
+  def validate_admin_token
+    unless insamling.admin_token == admin_token_in_params
+      redirect_to insamling_url(insamling)
+    end
+  end
+
+  def insamling
+    @insamling ||= Insamling.find(params[:insamling_id])
+  end
+
   def need_params
     params.require(:need).permit(:title, :description)
+  end
+
+  def admin_token_in_params
+    params[:admin_token]
   end
 end
--- spec/controllers/needs_controller_spec.rb
+++ spec/controllers/needs_controller_spec.rb
@@ -8,6 +8,7 @@ describe NeedsController do
         insamling = create(:insamling)
         params = {
           insamling_id: insamling.id,
+          admin_token: insamling.admin_token,
           need: {
             title: "this is needed",
             description: "And a descition"
@@ -23,6 +24,7 @@ describe NeedsController do
         insamling = create(:insamling)
         params = {
           insamling_id: insamling.id,
+          admin_token: insamling.admin_token,
           need: {
             title: "this is needed",
             description: "And a descition"
@@ -34,41 +36,79 @@ describe NeedsController do
         expect(response).to redirect_to "/insamlings/#{ insamling.id }/admins/#{ insamling.admin_token }"
       end
     end
+
+    context "with invalid params" do
+      it "redirects to the insamling" do
+        insamling = create(:insamling)
+        params = {
+          insamling_id: insamling.id,
+          admin_token: "invalid_token",
+          need: {
+            title: "this is needed",
+            description: "And a descition"
+          }
+        }
+
+        post :create, params
+
+        expect(response).to redirect_to("/insamlings/#{ insamling.id }")
+      end
+    end
   end
 
   describe "#destroy" do
-    it "deletes the need" do
-      insamling = create_insamling_with_one_need
-      params = {
-        insamling_id: insamling.id,
-        id: insamling.needs.first.id
-      }
+    context "with valid parameters" do
+      it "deletes the need" do
+        insamling = create_insamling_with_one_need
+        params = {
+          insamling_id: insamling.id,
+          id: insamling.needs.first.id,
+          admin_token: insamling.admin_token
+        }
 
-      expect { delete :destroy, params }.
-        to change { Need.count }.from(1).to(0)
+        expect { delete :destroy, params }.
+          to change { Need.count }.from(1).to(0)
+      end
+
+      it "removes the need from the insamling" do
+        insamling = create_insamling_with_one_need
+        params = {
+          insamling_id: insamling.id,
+          id: insamling.needs.first.id,
+          admin_token: insamling.admin_token
+        }
+
+        expect { delete :destroy, params }.
+          to change { Need.count }.from(1).to(0)
+      end
+
+      it "redirects to the admin page" do
+        insamling = create_insamling_with_one_need
+        params = {
+          insamling_id: insamling.id,
+          id: insamling.needs.first.id,
+          admin_token: insamling.admin_token
+        }
+
+        delete :destroy, params
+
+        expect(response).to redirect_to "/insamlings/#{ insamling.id }/admins/#{ insamling.admin_token }"
+      end
     end
 
-    it "removes the need from the insamling" do
-      insamling = create_insamling_with_one_need
-      params = {
-        insamling_id: insamling.id,
-        id: insamling.needs.first.id
-      }
+    context "with invalid params" do
+      it "redirects to the insamling" do
+        insamling = create_insamling_with_one_need
+        params = {
+          insamling_id: insamling.id,
+          id: insamling.needs.first.id,
+          admin_token: "invalid_token"
+        }
 
-      expect { delete :destroy, params }.
-        to change { Need.count }.from(1).to(0)
-    end
+        post :destroy, params
 
-    it "redirects to the admin page" do
-      insamling = create_insamling_with_one_need
-      params = {
-        insamling_id: insamling.id,
-        id: insamling.needs.first.id
-      }
-
-      delete :destroy, params
-
-      expect(response).to redirect_to "/insamlings/#{ insamling.id }/admins/#{ insamling.admin_token }"
+        expect(response).to redirect_to("/insamlings/#{ insamling.id }")
+      end
     end
 
     def create_insamling_with_one_need
