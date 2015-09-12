5853deb068cf4287496e7f707b27c16816be7f1f
Tobias Talltorp <tobias@talltorp.se>
2015-09-12 22:09:55 +0200

    Add new need in its own endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "welcome#show"
 
   resources :insamlings, only: [:new, :show, :create, :update] do
-    resources :needs, only: [:create, :destroy]
+    resources :needs, only: [:new, :create, :destroy]
     resources :admins, only: [:show]
   end
 end
